b726ad96fcd81af0a8616c0e39c30d8893fbdfc8
Julian Tellez <60985751+jtellez93@users.noreply.github.com>
2020-07-04 22:40:56 -0500

    Avance

--- 3-Getting and Cleaning Data/Scripts/swirl.R
+++ 3-Getting and Cleaning Data/Scripts/swirl.R
@@ -9,8 +9,109 @@ swirl()
 ### leccion 1: Manipulating Data with dplyr
 
 
+mydf <- read.csv(path2csv, stringsAsFactors = FALSE)
+dim(mydf)
+head(mydf)
+
+library(dplyr)
+packageVersion("dplyr")
+
+cran <- tbl_df(mydf)
+rm(mydf)
+
+?tbl_df # ahora se debe usar as_tibble()
+cran
+
+# Específicamente, dplyr proporciona cinco 'verbos' 
+# que cubren las tareas más fundamentales de manipulación de datos: 
+# seleccionar (), filtrar (), organizar (), mutar () y resumir ().
+# select(), filter(), arrange(), mutate(), and summarize()
+
+?select # permite seleccionar variables de interes
+select(cran, ip_id, package, country)
+
+5:20 # secuencia convencional
+
+# dplyr tambien se puede usar ":" con las columnas que deseo ver 
+
+select(cran, r_arch:country) # en el DF cran, seleccione columnas 
+# desde r_arch hasta country
+
+# tambien puedo hacerlo en orden inverso
+select(cran, country:r_arch)
+
+cran # aun no he modificado el DF original
+
+# si quiero omitir columnas uso "-" y el nombre de la variable
+
+select(cran, -time)
+
+-5:20
+-(5:20)
+
+select(cran, -(X:size))
+
+# si quiero seleccionar filas utilizo filter()
+
+filter(cran, package == "swirl") # filtra la columna paquetes que sean iguales a swirl 
+
+# tambien puedo hacer varios filtros en una sola expresion
+
+filter(cran, r_version == "3.1.1", country == "US")
+
+# con filter() tambien puedo usar comparaciones binarias
+?Comparison
+
+filter(cran, r_version <= "3.0.2", country == "IN")
+
+# tambien se puede hacer uso de operadores boleanos
+filter(cran, country == "US" | country == "IN")
+filter(cran, size > 100500, r_os == "linux-gnu")
+
+
+# deteccion de NA's
+is.na(c(3, 5, NA, 10))
+!is.na(c(3, 5, NA, 10))
+
+filter(cran, !is.na(r_version))
+
+
+# para organizar uso arrange()
+cran2 <- select(cran, size:ip_id)
+
+arrange(cran2, ip_id) # por defecto arrange() organiza en orden ascendente
+arrange(cran2, desc(ip_id)) # en orden descendente se usa el argumento desc()
+
+# puedo organizar varias columnas a la vez y de diferente forma
+
+arrange(cran2, package, ip_id)
+arrange(cran2, country, desc(r_version), ip_id)
+
+
+
+# tambien puedo agregar variables a un DF con mutate()
+cran3 <- select(cran, ip_id, package, size)
+cran3
+
+# si quiero agregar una columna llamada size_mb que contenga el tamaño de descarga en mb
+mutate(cran3, size_mb = size / 2^20)
+
+# ahora otra columna con el tamaño de descarga en gb
+mutate(cran3, size_mb = size / 2^20, size_gb = size_mb / 2^10)
+
+# se pueden agregar columnas con diferentes operaciones
+mutate(cran3, correct_size = size + 1000)
+
+
+# para resumir la informacion del DF utilizo summarize()
+# colapsa el df en una sola fila, (total por columna)
+summarize(cran, avg_bytes = mean(size))
 
 
 ### leccion 2: Grouping and Chaining with dplyr
+
+
+
+
 ### leccion 3: Tidying Data with tidyr
 ### leccion 4: Dates and Times with lubridate
