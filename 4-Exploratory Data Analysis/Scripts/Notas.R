dfba1cf8946e357bd8cdbcd699ed9d8b724ffe79
Julian Tellez <60985751+jtellez93@users.noreply.github.com>
2020-07-21 11:12:54 -0500

    nuevo curso

---
+++ 4-Exploratory Data Analysis/Scripts/Notas.R
@@ -0,0 +1,337 @@
+
+### Reading mySQL ####
+
+# los datos estan estructurados en bases de datos
+# en bases de datos con tablas
+# en tablas con campos
+
+# cada campo es una columna (variable)
+# cada fila es llamada record (observacion)
+
+install.packages("RMySQL")
+install.packages("DBI")
+library(DBI)
+library(RMySQL)
+
+
+ucscDb <- dbConnect(MySQL(), user = "genome",
+                    host = "genome-mysql.cse.ucsc.edu")
+
+result <- dbGetQuery(ucscDb, "show databases;"); dbDisconnect(ucscDb)
+
+# es imortante desconectarse del servidor antes 
+# de empezar a analizar la informacion
+
+
+result # en este objeto queda guardada una lista con todas las bases de datos 
+# disponibles en el MySQL server, ubicadas en la direccion host indicadas
+
+
+# hg19 es una construccion particular del genoma humano
+hg19 <- dbConnect(MySQL(), user = "genome", db = "hg19",
+                    host = "genome-mysql.cse.ucsc.edu")
+
+alltables <- dbListTables(hg19) # observamos todas las tablas que hay en hg19
+length(alltables)
+
+alltables[1:5]
+
+dbListFields(hg19, "affyU133Plus2") # si suponemos que conocemos que tabla nos interesa
+# podemos ver los campos de esa tabla con esta funcion
+# tabla corresponde a dataframe y campo corresponde a columna
+
+dbGetQuery(hg19, "select count(*) from affyU133Plus2")
+# si deseo saber el numero de filas de este dataframe utilizo este comando
+
+
+## Read from the table 
+# con la funcion deReadTable() podemos leer una tabla de la conexion con MySQL server
+# estamos leyendo el df affyU133Plus2 de la base de datos hg19
+affyData <- dbReadTable(hg19, "affyU133Plus2")
+head(affyData)
+
+
+## Select a specific subset
+# aqui sleccionamos de la base de datos hg19, de la tabla affyU133Plus2
+# un subconjunto en el que se cumplen que los valores de misMatches estan entre 1 y 3
+
+# primero se crea la consulta
+query <- dbSendQuery(hg19, "select * from affyU133Plus2 where misMatches between 1 and 3")
+
+# luego buscamos la consulta creada
+affyMis <- fetch(query); quantile(affyMis$misMatches)
+
+
+# tambien podemos buscar la consulta con un tamaño n deseado, 
+# n hace referencia al numero de columnas
+affyMisSmall <- fetch(query, n = 10); dbClearResult(query)
+dim(affyMisSmall)
+
+# con dbclearResult() liberamos el objeto query para evitar agotar
+# los recursos (memoria, descriptores de archivos, etc.)
+
+dbDisconnect(hg19)
+
+
+
+### Reading HDF5 ####
+# Hierarchical Data Format
+# Formato de datos jerárquico
+
+# este formato es usado para guardar datasets grandes
+# Conjuntos de datos, que son matrices multidimensionales de tipo homogéneo
+# Grupos, que son estructuras de contenedor que pueden contener datasets y otros grupos
+
+
+### Reading from the Web ####
+
+## Webscraping o raspado web
+# el web scraping se enfoca en la transformación de datos sin estructura
+# en la web (como el formato HTML) en datos estructurados
+# que pueden ser almacenados y analizados en una base de datos central,
+# en una hoja de cálculo o en alguna otra fuente de almacenamiento.
+
+# Alguno de los usos del web scraping son
+# la comparación de precios en tiendas, 
+# la monitorización de datos relacionados con el clima de cierta región,
+# la detección de cambios en sitios webs
+# y la integración de datos en sitios webs.
+# También es utilizado para obtener información relevante
+# de un sitio a través de los rich snippets.
+
+
+## Getting data off webpages - readLines()
+# la funcion readLines() permiter leer codigo HTML de internet
+
+con <- url("https://scholar.google.com/citations?hl=es&user=HI-I6C0AAAAJ")
+htmlCode <- readLines(con, warn = F) # lemos el codigo HTML
+# el argumento warn omite el warning cuando detecta
+# que no encuentra linea final en la url que indicamos
+
+close(con) # permite cerrar la conexion,
+# debo hacerlo antes de empezar a trabajar con los datos
+
+htmlCode # muestra el codigo HTML
+
+
+
+### Parsing with XML ###
+library(RCurl)
+library(XML)
+
+url <- "https://scholar.google.com/citations?hl=es&user=HI-I6C0AAAAJ"
+
+xData <- getURL(url)
+html <- htmlTreeParse(xData, useInternalNodes = T)
+
+xpathSApply(html, "//title", xmlValue)
+
+xpathSApply(html, "//td[@class='gsc_a_c']", xmlValue)
+xpathSApply(html, "//td[@class='gsc_a_y']", xmlValue)
+
+
+### Get from the httr package ####
+
+library(httr)
+
+html2 <- GET(url)
+content2 <- content(html2, as = "text")
+parsedHtml <- htmlParse(content2, asText = TRUE)
+
+xpathSApply(parsedHtml, "//title", xmlValue)
+
+xpathSApply(parsedHtml, "//td[@class='gsc_a_c']", xmlValue)
+xpathSApply(parsedHtml, "//td[@class='gsc_a_y']", xmlValue)
+
+
+
+### Accesing websites with passwords #### 
+
+pg1 <- GET("http://httpbin.org/basic-auth/user/passwd")
+pg1
+
+# Response [http://httpbin.org/basic-auth/user/passwd]
+# Date: 2020-07-06 03:03
+# Status: 401
+# Content-Type: <unknown>
+#         <EMPTY BODY>
+# el status indica que no se ha podido acceder
+#dado que requiere logearse
+
+
+# con el paquete HTTR realmente puede autenticarse 
+# para los sitios web, y puede hacerlo
+# asignando a esto lo que llamaremos el identificador.
+pg2 <- GET("http://httpbin.org/basic-auth/user/passwd",
+           authenticate("user","passwd"))
+# en los argumentos de authenticate 
+#van las credenciales para logearse, usuario y contraseña
+pg2
+# Response [http://httpbin.org/basic-auth/user/passwd]
+# Date: 2020-07-06 03:06
+# Status: 200
+# Content-Type: application/json
+# Size: 47 B
+# {
+#         "authenticated": true, 
+#         "user": "user"
+# }
+
+names(pg2)
+
+
+### Using handles ####
+
+google <- handle("http://google.com")
+
+pg1 <- GET(handle = google, path = "/")
+pg2 <- GET(handle = google, path = "search")
+
+
+
+### Reading data from APIs
+# APIs = aplication programming interfaces
+# como ejemplo se trabajara con twitter
+
+# inicialmente debo crear una cuenta en developer.twitter
+
+
+
+### algunas funciones utiles ####
+
+ceiling(3.475) # redondea hacia arriba
+# [1] 4
+
+floor(3.475) # redondea hacia abajo
+# [1] 3
+
+round(3.475, digits = 2) # redondea a un numero especifico de decimales
+# [1] 3.48
+
+signif(3.475, digits = 2) # redondea a un numero especifico de digitos significativos
+# [1] 3.5
+
+
+merge() # Combine dos df por columnas comunes 
+# o nombres de fila, o realice otras versiones de operaciones 
+# de unión de bases de datos.
+
+join() # permite hacer cruze de tablas, busquedas por una columna en comun
+
+
+
+
+### Editing text Variables  ####
+
+
+fileUrl <- "https://data.baltimorecity.gov/api/views/dz54-2aru/rows.csv?accesType=DOWNLOAD"
+download.file(fileUrl, "./Data/datos_sem4_video1.csv", method = "curl")
+
+cameraData <- read.csv("./Data/datos_sem4_video1.csv")
+names(cameraData)
+
+# la funcion tolower() Traduce caracteres en vectores de caracteres,
+# en particular de mayúsculas a minúsculas o viceversa.
+tolower(names(cameraData))
+
+
+# la function strsplit() Divide los elementos de un vector
+# de caracteres x en subcadenas de acuerdo con las coincidencias 
+# con la subcadena dividida dentro de ellos.
+
+# divide texto de acuerdo con el separador que se especifique
+# en el segundo argumento
+
+splitnames <- strsplit(names(cameraData), split = "\\.")
+
+splitnames[[5]]
+# [1] "intersection"
+
+splitnames[[6]]
+# [1] "Location" "1"
+
+splitnames[[7]]
+# [1] "X2010"         "Census"        "Neighborhoods"
+
+
+
+mylist <- list(letters = c("A", "b", "c"), 
+               numbers = 1:3, matrix(1:25, ncol = 5))
+head(mylist)
+
+# primer elemento de la lista
+mylist[1] 
+mylist$letters
+mylist[[1]]
+
+
+# reemplazar caracteres en un a cadena de texto
+testName <- "this_is_a_test"
+
+# la funcion sub() reemplaza elprimer argumento que coincida 
+# con el especificado con uno que tambien se especifica
+sub("_", "", testName)
+
+# la funcion gsub() reemplaza todo lo que conincida 
+gsub("_", "", testName)
+
+
+
+# la funcion nchar() cuenta el numero de caracteres de una cadena 
+
+nchar("julian tellez")
+# [1] 13
+
+# la funcion substr() extrae caracteres de un inicio a un fin 
+substr("julian tellez", 1, 6)
+# [1] "julian"
+
+# la funcion paste0() concatena sin separar palabras
+paste0("julian", "tellez")
+# [1] "juliantellez"
+
+# la funcion strtrim() recorta los espacios deja solo los caracteres
+strtrim("julian    ", width = 6)
+
+
+
+### Working whit dates ####
+
+d2 <- Sys.Date()
+d2
+
+class(d2)
+
+# formatting dates
+# %d = dia (0-31)
+# %a = dia de la semana abreviado
+# %A = dia de la semana completo
+# %m = mes (0-12)
+# %b = mes abreviado
+# %B = mes completo
+# %y = año en 2 digitos
+# %Y = año en 4 digitos
+
+format(d2, "%a %b %d")
+
+
+# creating dates
+
+x <- c("1feb1960", "2feb1960", "31mar1960", "30jul19460")
+z <- as.Date(x, "%d%b%Y")
+z
+
+z[1]-z[2]
+as.numeric(z[1]-z[2])
+
+
+# converting to julian
+weekdays(d2)
+months(d2)
+
+julian(d2) # extrae partes de un objeto tipo fecha, devuelve el valor desde el origen
+
+
+# lubridate
+
+
---
+++ 4-Exploratory Data Analysis/Scripts/swirl.R
@@ -0,0 +1,412 @@
+
+# paquete swirl
+packageVersion("swirl")
+library(swirl)
+install_from_swirl("Getting and Cleaning Data")
+swirl()
+
+
+### leccion 1: Manipulating Data with dplyr ####
+
+
+mydf <- read.csv(path2csv, stringsAsFactors = FALSE)
+dim(mydf)
+head(mydf)
+
+library(dplyr)
+packageVersion("dplyr")
+
+cran <- tbl_df(mydf)
+rm("mydf")
+
+?tbl_df # ahora se debe usar as_tibble()
+cran
+
+# Específicamente, dplyr proporciona cinco 'verbos' 
+# que cubren las tareas más fundamentales de manipulación de datos: 
+# seleccionar (), filtrar (), organizar (), mutar () y resumir ().
+# select(), filter(), arrange(), mutate(), and summarize()
+
+?select # permite seleccionar variables de interes
+select(cran, ip_id, package, country)
+
+5:20 # secuencia convencional
+
+# dplyr tambien se puede usar ":" con las columnas que deseo ver 
+
+select(cran, r_arch:country) # en el DF cran, seleccione columnas 
+# desde r_arch hasta country
+
+# tambien puedo hacerlo en orden inverso
+select(cran, country:r_arch)
+
+cran # aun no he modificado el DF original
+
+# si quiero omitir columnas uso "-" y el nombre de la variable
+
+select(cran, -time)
+
+-5:20
+-(5:20)
+
+select(cran, -(X:size))
+
+# si quiero seleccionar filas utilizo filter()
+
+filter(cran, package == "swirl") # filtra la columna paquetes que sean iguales a swirl 
+
+# tambien puedo hacer varios filtros en una sola expresion
+
+filter(cran, r_version == "3.1.1", country == "US")
+
+# con filter() tambien puedo usar comparaciones binarias
+?Comparison
+
+filter(cran, r_version <= "3.0.2", country == "IN")
+
+# tambien se puede hacer uso de operadores boleanos
+filter(cran, country == "US" | country == "IN")
+filter(cran, size > 100500, r_os == "linux-gnu")
+
+
+# deteccion de NA's
+is.na(c(3, 5, NA, 10))
+!is.na(c(3, 5, NA, 10))
+
+filter(cran, !is.na(r_version))
+
+
+# para organizar uso arrange()
+cran2 <- select(cran, size:ip_id)
+
+arrange(cran2, ip_id) # por defecto arrange() organiza en orden ascendente
+arrange(cran2, desc(ip_id)) # en orden descendente se usa el argumento desc()
+
+# puedo organizar varias columnas a la vez y de diferente forma
+
+arrange(cran2, package, ip_id)
+arrange(cran2, country, desc(r_version), ip_id)
+
+
+
+# tambien puedo agregar variables a un DF con mutate()
+cran3 <- select(cran, ip_id, package, size)
+cran3
+
+# si quiero agregar una columna llamada size_mb que contenga el tamaño de descarga en mb
+mutate(cran3, size_mb = size / 2^20)
+
+# ahora otra columna con el tamaño de descarga en gb
+mutate(cran3, size_mb = size / 2^20, size_gb = size_mb / 2^10)
+
+# se pueden agregar columnas con diferentes operaciones
+mutate(cran3, correct_size = size + 1000)
+
+
+# para resumir la informacion del DF utilizo summarize()
+# colapsa el df en una sola fila, (total por columna)
+summarize(cran, avg_bytes = mean(size))
+
+
+### leccion 2: Grouping and Chaining with dplyr ####
+
+library(dplyr)
+
+cran <- tbl_df(mydf)
+rm("mydf")
+cran
+
+# puedo agrupar variables deacuerdo a categorias con group_by()
+
+?group_by
+
+by_package <- group_by(cran, package)
+by_package
+
+# una vez agrupados es mas interesante usar summarize para calcular por grupos
+
+summarize(by_package, mean(size))
+
+# tambien puedo sacar varios resumenes y guardarlos en un objeto
+pack_sum <- summarize(by_package,
+                      count = n(),
+                      unique = n_distinct(ip_id),
+                      countries = n_distinct(country),
+                      avg_bytes = mean(size))
+
+pack_sum
+
+
+# si quiero saber el numero de paquetes deacuerdo al numero de 
+# descargas que estan por encima del 99% uso la funcion quantile()
+quantile(pack_sum$count, probs = 0.99)
+
+#los que tienen mas de 679 estan en el cuantil 99% 
+
+top_counts <- filter(pack_sum, count > 679)
+top_counts
+
+View(top_counts)
+
+top_counts_sorted <- arrange(top_counts, desc(count))
+
+View(top_counts_sorted)
+
+# cuantil 99% para lal columna unique
+quantile(pack_sum$unique, probs = 0.99)
+
+# son las filas conmas de 465 
+top_unique <- filter(pack_sum, unique > 465)
+View(top_unique)
+
+top_unique_sorted <- arrange(top_unique, desc(unique))
+View(top_unique_sorted)
+
+View(result3)
+
+
+cran %>%
+        select(ip_id, country, package, size) %>%
+        print
+
+cran %>%
+        select(ip_id, country, package, size) %>%
+        mutate(size_mb = size / 2^20) %>%
+        print
+
+cran %>%
+        select(ip_id, country, package, size) %>%
+        mutate(size_mb = size / 2^20) %>%
+        filter(size_mb <= 0.5) %>%
+        print
+
+cran %>%
+        select(ip_id, country, package, size) %>%
+        mutate(size_mb = size / 2^20) %>%
+        filter(size_mb <= 0.5) %>%
+        arrange(desc(size_mb)) %>%
+        print
+
+### leccion 3: Tidying Data with tidyr ####
+library(dplyr)
+library(tidyr)
+
+3
+students
+# la variable sexo se encuentra en dos columnas 
+# para reunir la variable en una sola columna puedo usar la funcion gather()
+?gather
+gather(students, key = sex, value = count, -grade)
+
+# actualmente se recomienda el uso de pivot_longer()
+
+students2
+# la variable sexo esta dividida en 4 columnas debe reunirse en una sola 
+gather(students2, key =  sex_class, value = count, -grade)
+res <- gather(students2, sex_class, count, -grade)
+res
+
+# ahora tengo la variable sexo y sexclass en una columna debo separarlas
+# siempre debe estar una variable en una columna
+# para este caso usamos separate()
+# Separe una columna de caracteres en varias columnas con una expresión regular o ubicaciones numéricas
+
+?separate
+separate(data = res, col = sex_class, into = c("sex", "class"))
+
+# da el mismo resultado si lo hacemos encadenando con %>%
+
+students2 %>%
+        gather( sex_class, count, -grade) %>%
+        separate( sex_class, c("sex", "class")) %>%
+        print
+
+
+# en este caso hay que reunir las clases por grado 
+students3
+students3 %>%
+        gather( class, grade, class1:class5 , na.rm = TRUE) %>%
+        print
+
+# el siguiente paso requiere de spread()
+?spread
+# Distribuya un par clave-valor en varias columnas.
+
+students3 %>%
+        gather(class, grade, class1:class5, na.rm = TRUE) %>%
+        spread( test, grade) %>%
+        print
+
+
+library(readr)
+parse_number("class5")
+# Esto elimina los caracteres no numéricos antes o después del 
+# primer número. La marca de agrupación especificada por la 
+# configuración regional se ignora dentro del número.
+
+# si deseo cambiar las palabras class_1, class_2... 
+#utilizo esta funcion para que solo quede la parte numerica 1,2...
+
+students3 %>%
+        gather(class, grade, class1:class5, na.rm = TRUE) %>%
+        spread(test, grade) %>%
+        mutate(class = parse_number(class)) %>%
+        print
+
+# otro problema con messy data
+
+students4
+# los datos estan organizados por variable y por observacion
+# sin enbargo hay observaciones repetidas para id, nombre y sex, por tanto 
+# deben separarse en tablas diferentes
+
+student_info <- students4 %>%
+        select( id, name, sex)%>%
+        print
+
+student_info <- students4 %>%
+        select(id, name, sex) %>%
+        unique() %>%
+        print
+
+gradebook <- students4 %>%
+        select(id, class, midterm, final) %>%
+        print
+
+# para relacionar tablas tenemos este ejemplo con dos tablas
+passed
+failed
+
+# primero creamos una columna llamada status con la 
+# etiqueta passed o failed a los estudientes que aprobaron o perdieron
+
+passed <- passed %>% mutate(status = "passed")
+failed <- failed %>% mutate(status = "failed")
+
+# para unir estas dos tablas usamos bind_rows()
+# esta funcion sirve para para vincular muchos df en uno.
+?bind_rows
+
+bind_rows(passed, failed)
+
+
+# I've created a variable called 'sat' in your workspace, which contains data
+# on all college-bound seniors who took the SAT exam in 2013
+
+sat
+sat %>%
+        select(-contains("total")) %>%
+        gather(part_sex, count, -score_range) %>%
+        separate(part_sex, c("part", "sex")) %>%
+        print
+
+
+sat %>%
+        select(-contains("total")) %>%
+        gather(part_sex, count, -score_range) %>%
+        separate(part_sex, c("part", "sex")) %>%
+        group_by(part, sex) %>%
+        mutate(total = sum(count), prop = count / total) %>% 
+        print
+
+
+### leccion 4: Dates and Times with lubridate ####
+
+Sys.getlocale("LC_TIME") # muestra configuracion local de tiempo
+library(lubridate)
+?lubridate
+help(package = lubridate)
+# Lubridate proporciona herramientas que facilitan el análisis
+# y la manipulación de fechas
+
+today() # devuelve la fecha del dia actual
+
+this_day <- today()
+this_day
+
+year(this_day); month(this_day); day(this_day)
+
+wday(this_day) # devuelve el dia de la semanba, iniciando el domingo = 1
+# si quiero ver el orden de los dias uso el argumento label = TRUE
+
+wday(this_day, label = TRUE)
+
+now() # devuelve fecha y hora del momento que se ejecuta
+
+this_moment <- now()
+this_moment
+
+hour(this_moment); minute(this_moment); second(this_moment)
+
+
+# para trabajar formatos de fecha uso las funciones
+# ymd(), dmy(), hms(), ymd_hms()... etc
+# y = año, m = mes, d = dia
+
+
+my_date <- ymd("1989-05-17")
+my_date
+
+class(my_date)
+
+ymd("1989 May 17")
+mdy("March 12, 1975")
+dmy(25081985)
+ymd("192012")
+ymd("1920/1/2")
+
+dt1
+ymd_hms(dt1)
+hms("03:22:14")
+
+
+dt2
+ymd(dt2)
+
+# la funcion update permite modificar valores de un objeto tipo fecha
+update(this_moment, hours = 8, minutes = 34, seconds = 55)
+this_moment
+
+this_moment <- update(this_moment, hours = 8, minutes = 34, seconds = 55)
+this_moment
+
+
+nyc <- now("America/New_York") # a now tambien puedo agregarle zona horaria
+nyc
+
+nyc + days(2) # de esta forma sumo dos dias a una fehca establecida
+
+depart <- nyc + days(2)
+depart
+
+depart <- update(depart, hours = 17, minutes = 34)
+depart
+
+arrive <- depart + hours(15) + minutes(50)
+
+
+?with_tz
+# with_tz devuelve una fecha y hora como aparecería
+# en una zona horaria diferente. 
+# El momento real del tiempo medido no cambia, 
+# solo el huso horario en el que se mide
+
+arrive <- with_tz(arrive, "Asia/Hong_Kong")
+arrive
+
+
+last_time <- mdy("June 17, 2008", tz = "Singapore")
+last_time
+
+# Intervalo () crea un objeto Intervalo con las fechas de inicio y
+# finalización especificadas. 
+# Si la fecha de inicio ocurre antes de la fecha de finalización,
+# el intervalo será positivo. De lo contrario, será negativo.
+?interval
+
+how_long <- interval(last_time, arrive)
+how_long
+
+as.period(how_long) # muestra la amplitud del intervalo
+
+stopwatch()
