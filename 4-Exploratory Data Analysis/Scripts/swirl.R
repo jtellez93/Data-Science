c262776fa470d94d1f9f4199f4b488ab14a7c7f4
Julian Tellez <60985751+jtellez93@users.noreply.github.com>
2020-08-01 12:49:56 -0500

    update

--- 4-Exploratory Data Analysis/Scripts/swirl.R
+++ 4-Exploratory Data Analysis/Scripts/swirl.R
@@ -201,4 +201,71 @@ plot(airquality$Temp, airquality$Ozone, main = "Ozone and Temperature")
 mtext("Ozone and Weather in New York City", outer = TRUE)
 
 
+# lección 6: Lattice Plotting System --------------------------------------
+
+1
+head(airquality)
+
+xyplot(Ozone~Wind, data = airquality)
+xyplot(Ozone ~ Wind, data = airquality, pch=8, col="red", main="Big Apple Data")
+
+xyplot(Ozone ~ Wind | as.factor(Month), data = airquality, layout=c(5,1))
+
+3
+
+xyplot(Ozone ~ Wind | Month, data = airquality, layout=c(5,1))
+
+p <- xyplot(Ozone~Wind,data=airquality)
+
+print(p)
+names(p)
+
+mynames[myfull]
+
+p[["formula"]]
+
+p[["x.limits"]]
+
+table(f)
+
+xyplot(y ~ x | f, layout = c(2, 1))
+
+v1
+v2
+myedit("plot1.R")
+2
+3
+source(pathtofile("plot1.R"),local=TRUE)
+3
+myedit("plot2.R")
+
+source(pathtofile("plot2.R"),local=TRUE)
+2
+
+str(diamonds)
+
+
+table(diamonds$color)
+table(diamonds$color, diamonds$cut)
+2
+
+myedit("myLabels.R")
+
+source(pathtofile("myLabels.R"),local=TRUE)
+
+xyplot(price~carat|color*cut,data=diamonds,strip=FALSE,pch=20,xlab=myxlab,ylab=myylab,main=mymain)
+1
+xyplot(price~carat|color*cut,data=diamonds,pch=20,xlab=myxlab,ylab=myylab,main=mymain)
+4
+2
+1
+2
+1
+1
+2
+2
+
+
+
+
 
